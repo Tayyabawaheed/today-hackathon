fe6b8a7225b751b062bbb75a3d1790a52e2f3670
Tayyabawaheed <124914725+Tayyabawaheed@users.noreply.github.com>
2023-08-27 19:46:58 +0500

    Add files via upload

---
+++ configue.js
@@ -0,0 +1,24 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD1p65JpFxAWT7c6B63uNWJ3DnH2sIgB18",
+    authDomain: "hackathon-app-154ac.firebaseapp.com",
+    projectId: "hackathon-app-154ac",
+    storageBucket: "hackathon-app-154ac.appspot.com",
+    messagingSenderId: "652552242185",
+    appId: "1:652552242185:web:54cd14757edcfe8068a4d8",
+    measurementId: "G-T5LYF8X3EB"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+
+
+
+
+
+
+
+
+
+
+ 
